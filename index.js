b913301c25818c6b663a49986eb324635f7d3c88
olha nevzghodova <onevz.alex@gmail.com>
2022-10-07 21:17:55 +0300

    delete default value in prompt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const num = +prompt('Enter a number, please', '');
-const number = +prompt('Enter a second number, please', '');
+const num = +prompt('Enter a number, please');
+const number = +prompt('Enter a second number, please');
 const sum = num + number;
 
 alert(sum);
